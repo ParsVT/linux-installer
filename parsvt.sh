2ff321ec8d5114376d0c55afa5b3adbde4a049ab
Mohammad Hadadpour <mamad.mh11@gmail.com>
2025-03-04 10:23:42 +0330

    Update parsvt.sh

--- parsvt.sh
+++ parsvt.sh
@@ -3,7 +3,7 @@
 # Program: ParsVT CRM Installation Script
 # Developer: Hamid Rabiei, Mohammad Hadadpour
 # Release: 1397-12-10
-# Update: 1403-12-09
+# Update: 1403-12-14
 # #########################################
 set -e
 shecanProDNS1="178.22.122.101"
@@ -501,11 +501,20 @@ sqlConf() {
 			if command -V "mysql" &>/dev/null | grep -q "MariaDB"; then
 				mv -n /etc/my.cnf.d/disable_mysql_strict_mode.cnf /etc/my.cnf.d/disable_mysql_strict_mode.cnf.old
 				wget -q http://$primarySite/modules/addons/easyservice/Installer/sqlconf.txt -O /etc/my.cnf.d/disable_mysql_strict_mode.cnf
+				if [ "$major" = "7" ] || [ "$major" = "8" ] || [ "$major" = "9" ] || [ "$major" = "10" ]; then
+					systemctl restart mariadb
+				else
+					service mariadb restart
+				fi
 			else
 				mv -n /etc/my.cnf.d/disable_mysql_strict_mode.cnf /etc/my.cnf.d/disable_mysql_strict_mode.cnf.old
 				wget -q http://$primarySite/modules/addons/easyservice/Installer/sqlconf2.txt -O /etc/my.cnf.d/disable_mysql_strict_mode.cnf
+				if [ "$major" = "7" ] || [ "$major" = "8" ] || [ "$major" = "9" ] || [ "$major" = "10" ]; then
+					systemctl restart mysqld
+				else
+					service mysqld restart
+				fi
 			fi
-			restartDatabase
 			echo -e "${Green}DONE!${Color_Off}\n"
 		elif [ "$sqlconfig" = "n" ] || [ "$sqlconfig" = "no" ] || [ "$sqlconfig" = "N" ] || [ "$sqlconfig" = "No" ] || [ "$sqlconfig" = "NO" ] || [ "$sqlconfig" = "0" ]; then
 			echo -e "${Yellow}OK!${Color_Off}\n"
